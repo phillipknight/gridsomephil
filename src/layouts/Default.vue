fdbe387475d516a7a4847b80fb44a1662704582a
phillipknight <phillipkknight@gmail.com>
2019-09-13 23:10:00 -0400

    implement theme switcher, begin adding theme vars

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,6 +5,7 @@
         <g-link to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
       <top-navigation></top-navigation>
+      <toggle-theme></toggle-theme>
     </header>
     <slot />
   </div>
@@ -17,13 +18,45 @@ query {
   }
 }
 </static-query>
+<script>
+import ToggleTheme from '~/components/ToggleTheme.vue'
 
+export default {
+  components: {
+    ToggleTheme
+  }
+}
+</script>
 <style>
+
+
 body {
+  --header-bg: #ff7e00;
+  --header-link-color: #ffffff;
+  --header-hover:#000000;
+  --nav-bottom: #ff7e00;
+  --nav-bottom-hover:#000000;
+  --body-bg: #ffffff;
+  --body-copy: #000000;
+
+  
   font-family: sans-serif;
   margin: 0;
   padding: 0;
   line-height: 1.5;
+  background: var(--body-bg);
+  color: var(--body-copy)
+}
+
+body[data-theme="dark"] {
+  --header-bg: #353535;
+  --header-link-color: #ffffff;
+  --header-hover:#00ff4c;
+  --nav-bottom: #00ff4c;
+  --nav-bottom-hover:#000000;
+  --body-bg: #000000;
+  --body-copy: #ffffff;
+
 }
 
 .layout {
@@ -39,11 +72,11 @@ body {
   height: 3rem;
   margin-bottom: 20px;
   font-family: sans-serif;
-  background-color: #ff7e00;
-  color: #ffffff;
+  background-color: var(--header-bg);
+  color: var(--header-link-color);
 }
 .header a {
-  color: #ffffff;
+  color: var(--header-link-color);
   text-decoration: none;
   text-transform: lowercase;
 }
@@ -57,15 +90,15 @@ body {
   height: 3rem;
   align-items: center;
   justify-content: center;
-  border-bottom: 0.5rem solid #ff7e00;
+  border-bottom: 0.5rem solid var(--nav-bottom);
   box-sizing: border-box;
   padding: 0.5rem 0.25rem 0 0.25rem;
 }
 
 .nav__link:hover,
 .nav__link:focus {
-  border-bottom-color: #000000;
-  color: #000000;
+  border-bottom-color: var(--nav-bottom-hover);
+  color: var(--header-hover);
 }
 
 .nav__link.active--exact,
@@ -74,5 +107,6 @@ body {
   border-bottom-color: #000000;
   color: #ffffff;
   cursor: default;
+  border-bottom: 0.5rem solid #ffffff;
 }
 </style>
